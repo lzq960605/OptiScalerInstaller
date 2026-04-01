8ebc6271d110984a081ea8b8d0740a3b0943886d
onehoon <onehoon@gmail.com>
2026-04-01 21:24:38 +0900

    refactor: update application version to 0.2.3

--- OptiScaler_Installer.py
+++ OptiScaler_Installer.py
@@ -66,7 +66,7 @@ except ModuleNotFoundError as e:
 import rtss_notice
 
  # Application Version
-APP_VERSION = "0.2.2"
+APP_VERSION = "0.2.3"
 # Install flow supports up to two detected GPUs. Dual-GPU requires explicit user selection.
 MAX_SUPPORTED_GPU_COUNT = 2
 
